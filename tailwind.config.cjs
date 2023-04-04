02f17281759fafc395a041a9cea0b7da5cd4156e
Faizzroi <roihanfaizz@gmail.com>
2023-04-04 21:23:59 +0700

    tugas slicing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-        gridTemplateColumns: {
-          'dashboard': 'repeat(auto-fill, minmax(470px, 1fr))',
-          'tablet': 'repeat(auto-fill, minmax(340px, 1fr))',
-        }
-
+      gridTemplateColumns: {
+        dashboard: "repeat(auto-fill, minmax(30%, 1fr))",
+        tablet: "repeat(auto-fill, minmax(40.75vw, 1fr))",
+      },
     },
   },
   plugins: [],
-}
+};
